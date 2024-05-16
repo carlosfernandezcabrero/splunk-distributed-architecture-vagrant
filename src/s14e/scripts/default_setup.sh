14aa85db8d9483f25a9a055330da979f4c00443a
Carlos Fernandez Cabrero <56631428+carlosfernandezcabrero@users.noreply.github.com>
2024-05-16 15:49:12 +0200

    Disable THP

--- src/s14e/scripts/default_setup.sh
+++ src/s14e/scripts/default_setup.sh
@@ -14,3 +14,22 @@ echo "vagrant hard nofile $1" >> /etc/security/limits.conf
 
 echo "vagrant soft nproc $2" >> /etc/security/limits.conf
 echo "vagrant hard nproc $2" >> /etc/security/limits.conf
+
+
+echo "[INFO] Configuring THP (Transparent HugePage) at boot time"
+#---------------------------------------------------
+sed -i "s/GRUB_CMDLINE_LINUX=\"/&transparent_hugepage=never /" /etc/default/grub
+
+source /etc/os-release
+
+rhel_ids=("rhel" "centos" "fedora")
+debian_ids=("debian" "ubuntu")
+
+if [[ " ${rhel_ids[@]} " =~ " ${ID} " ]]; then
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+elif [[ " ${debian_ids[@]} " =~ " ${ID} " ]]; then
+    update-grub
+else
+    echo "[ERROR] OS not supported"
+    exit 1
+fi
